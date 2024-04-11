557384dc7e5a39d1b654d83244439d676855e0bc
murageh <muragehdeveloper@gmail.com>
2024-04-11 16:53:23 +0300

    attempt to make responsive
    Signed-off-by: murageh <muragehdeveloper@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,18 +115,19 @@ function Home() {
     }, []);
 
     return (
-        <main className="flex h-screen flex-col items-center justify-center gap-y-10 px-24 py-10">
+        <main
+            className="flex h-screen max-w-5xl mx-auto flex-col items-center justify-center gap-y-10 px-2 md:px-24 py-2 md:py-10">
             {
                 loading ? (
                     <Loader loading={loading}/>
                 ) : null
             }
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                <p className="fixed left-0 top-0 hidden md:flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     Hi there.
                 </p>
                 <div
-                    className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                    className="ml-auto py-2 flex items-end justify-center lg:static bg-none">
                     <span className={`text-xs text-gray-400 dark:text-gray-500`}>
                         Web Gourmet - <span className="text-gray-500 dark:text-gray-400">Ask Me Anything</span>
                     </span>
@@ -134,17 +135,17 @@ function Home() {
             </div>
 
             <div
-                className={`my-auto relative flex flex-col ${hasSubmittedWebsite ? 'w-full' : 'w-8/12'} place-items-center justify-center items-center self-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[0]`}>
+                className={`my-auto relative flex flex-col ${hasSubmittedWebsite ? 'w-full' : 'w-11/12 md:w-8/12'} place-items-center justify-center items-center self-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[0]`}>
                 <div className="w-full px-3 h-full ">
                     I will answer any question regarding&nbsp;
                     <code className="font-mono font-bold">any website of your choice</code>
                 </div>
-                <div className="flex w-full justify-center items-center sticky">
+                <div className="flex w-full flex-col md:flex-row justify-center items-center sticky">
                     <InputField
                         autoFocus
                         name="url"
                         type="search"
-                        className="  w-full self-center p-4 text-left border border-gray-300 rounded-lg dark:border-neutral-800 dark:bg-zinc-800/30 dark:text-gray-100 md:w-8/12 lg:w-8/12"
+                        className=" w-full self-center p-4 text-left border border-gray-300 rounded-lg dark:border-neutral-800 dark:bg-zinc-800/30 dark:text-gray-100 md:w-8/12 lg:w-8/12"
                         placeholder="Enter a website URL"
                         ref={urlRef}
                         onKeyDown={handleKeyDown}
@@ -155,7 +156,7 @@ function Home() {
                         onClick={
                             hasSubmittedWebsite ? reset : handleSubmitUrl
                         }
-                        className="submit my-3 p-3 text-white bg-gradient-to-r from-primary to-secondary rounded-lg dark:from-neutral-700 dark:to-neutral-800 dark:bg-neutral-800/30 flex-grow hover:from-transparent hover:to-transparent hover:bg-secondary hover:text-black">
+                        className="submit mx-3 md:mx-0 my-0 md:my-3 p-3 self-start md:self-center text-white bg-gradient-to-r from-primary to-secondary rounded-lg dark:from-neutral-700 dark:to-neutral-800 dark:bg-neutral-800/30 flex-grow hover:from-transparent hover:to-transparent hover:bg-secondary hover:text-black">
                         {
                             hasSubmittedWebsite ?
                                 <span>Change&nbsp;Website&nbsp;URL&nbsp;&#x21bb;</span>
